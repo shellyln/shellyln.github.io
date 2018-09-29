a922cabfb4b3e3ea9c931b8dc8126ccf74d5f259
shellyln <shellyln@users.noreply.github.com>
2018-09-30 08:55:55 +0900

    Add files

---
+++ menneu/mkd.js
@@ -0,0 +1,11 @@
+const start = (async () => {
+    const buf = await menneu.render(Array.from(document.querySelectorAll('script[type="text/markdown"]'), x => x.innerHTML).join(' '), {}, {
+        rawInput: false,
+        inputFormat: 'md',
+        outputFormat: 'html',
+        title: 'Markdown example',
+        markdownBodyStyle: 'font-family: "Yu Gothic Medium", YuGothic, meiryo, "Microsoft JhengHei", "Microsoft YaHei", "SimHei", helvetica, arial, sans-serif;',
+    });
+    document.write(buf.toString());
+    document.close();
+});
