47271405a61d23ec137a89011c3af25d81f04818
shellyln <shellyln@users.noreply.github.com>
2018-09-30 22:41:18 +0900

    Edit playground

--- menneu/playground.js
+++ menneu/playground.js
@@ -1,5 +1,69 @@
 
-const AppState = {};
+const AppState = {
+    currentIndex: 0,
+};
+
+const exampleCodes = [{
+    name: 'Markdown',
+    value: './playground-md.md',
+    inputFormat: 'md',
+    dataFormat: 'object',
+    data: [1, 2, 3, 4, 5, 6],
+}, {
+    name: 'Billing (LSX)',
+    value: './billing.lsx',
+    inputFormat: 'lsx',
+    dataFormat: 'lisp',
+    data: `(#
+        (me (#
+            (name "Acme Inc.")
+            (addr1 "999 Kiely Blvd,")
+            (addr2 "")
+            (city "Santa Clara,")
+            (state "CA")
+            (zip "99999")
+        ))
+    
+        (billTo (#
+            (name "Foobar Inc.")
+            (addr1 "9999 N Fremont St,")
+            (addr2 "")
+            (city "Portland,")
+            (state "OR")
+            (zip "99999")
+        ))
+    
+        (date "2020-01-01")
+        (invoiceNo "X-1234-567890")
+        (subtotal 0)
+        (tax1 0)
+        (tax1Name "Sales Tax")
+        (tax1Rate 0.05)
+        (tax2 0)
+        (tax2Name "Excise Tax")
+        (tax2Rate 0.03)
+        (shipping 3.55)
+        (total 0)
+        (paid 15)
+        (totalDue 0)
+        (notes "note\nnote")
+    
+        (detail ($map ($range 0 42) (-> (v index) (#
+            (date "2019-12-01")
+            (description ($concat "$0.10 per GB of SSD storage (" (+ v 1) ")"))
+            (qty 4)
+            (rate 15.33)
+            (amount 0)
+        ))))
+    )`,
+}, {
+    name: 'HTML',
+    value: './playground-html.html',
+    inputFormat: 'html',
+    dataFormat: 'object',
+    data: [3, 5, 7, 11, 13],
+}];
+
 
 const escapeHtml = (s) => s
 .replace(/&/g, "&amp;")
@@ -49,6 +113,32 @@ const start = (async (text, cf, data) => {
 });
 
 
+class ExampleLoader extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {};
+    }
+
+    handleExampleSelected(i) {
+        this.props.loadExample(i);
+    }
+
+    render() {
+        return (lsx`
+        (Template
+            (select (@ (style (display "inline-block")
+                              (width "300px") )
+                       (onChange ${(e) => this.handleExampleSelected(e.target.value)}) )
+                ($=for ${exampleCodes}
+                    (option (@ (value $index)) ($get $data "name") )
+                )
+            )
+        )`);
+    }
+}
+
+
 class AceEditor extends React.Component {
     constructor(props, context) {
         super(props, context);
@@ -93,11 +183,12 @@ class App extends React.Component {
     }
 
     loadExample(i) {
+        AppState.currentIndex = i;
         const editor = AppState.AceEditor['editor'];
         editor.setValue(document.getElementById('md').innerHTML);
         editor.clearSelection();
 
-        fetch('./playground-md.md').then(response => {
+        fetch(exampleCodes[i].value).then(response => {
             response.text().then(x => {
                 editor.setValue(x);
             })
@@ -114,7 +205,11 @@ class App extends React.Component {
         this.setState({stretched: false});
         const editor = AppState.AceEditor['editor'];
 
-        start(editor.getValue())
+        const d = exampleCodes[AppState.currentIndex];
+        start(editor.getValue(), {
+            inputFormat: d.inputFormat,
+            dataFormat: d.dataFormat,
+        }, d.data)
         .then(html => {
             const doc = document.getElementById('root').contentWindow.document;
             doc.write(html);
@@ -127,6 +222,8 @@ class App extends React.Component {
         return (lsx`
         (Template
             (div (@ (style (padding "4px")))
+                (ExampleLoader  (@ (loadExample ${(i) => this.loadExample(i)}) ))
+                " "
                 (button (@ (style (textTransform "none"))
                            (className "waves-effect waves-light brown lighten-1 btn")
                            (onClick ${(e) => this.handleStretchedClick(e)}) ) "⇔")
@@ -152,6 +249,7 @@ window.lsx = liyad.LSX({
     jsx: React.createElement,
     jsxFlagment: React.Fragment,
     components: {
+        ExampleLoader,
         AceEditor,
         App,
     },
