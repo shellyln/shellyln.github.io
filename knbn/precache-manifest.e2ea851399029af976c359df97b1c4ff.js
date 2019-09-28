cdd8ee048bbee3e4e869b38b5c2e79ae35bb6f85
shellyln <shellyln@users.noreply.github.com>
2019-09-28 13:19:28 +0900

    fix deploy target dir

---
+++ knbn/precache-manifest.e2ea851399029af976c359df97b1c4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3266a8693c54fe5049535b83d2e5ae58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "754e48f9dbd684a046eb",
+    "url": "./static/css/2.5abeca67.chunk.css"
+  },
+  {
+    "revision": "ba4a14864b505817e84f",
+    "url": "./static/css/main.8f9516d5.chunk.css"
+  },
+  {
+    "revision": "754e48f9dbd684a046eb",
+    "url": "./static/js/2.84dcb565.chunk.js"
+  },
+  {
+    "revision": "ba4a14864b505817e84f",
+    "url": "./static/js/main.0c89a790.chunk.js"
+  },
+  {
+    "revision": "54cf88969ca72252a525",
+    "url": "./static/js/runtime-main.5575ecae.js"
+  }
+]);
--- knbn/service-worker.js
+++ knbn/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40f931e05139b6b5ef8ca7080fac522a.js"
+  "./precache-manifest.e2ea851399029af976c359df97b1c4ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
