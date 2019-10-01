f9e24b547c33f616a4798643d87dd7ffb3e185fc
shellyln <shellyln@users.noreply.github.com>
2019-10-02 02:45:32 +0900

    add app for staging

---
+++ knbn-stg/precache-manifest.73764158a3ea29aa7a876e8bfa500d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d598f87b10a4bf227530feff675b24be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e66f07814a7149e0e0f7",
+    "url": "./static/css/2.5abeca67.chunk.css"
+  },
+  {
+    "revision": "fbf678f254bdbc3fcb2a",
+    "url": "./static/css/main.e7a825d8.chunk.css"
+  },
+  {
+    "revision": "e66f07814a7149e0e0f7",
+    "url": "./static/js/2.5afefe75.chunk.js"
+  },
+  {
+    "revision": "fbf678f254bdbc3fcb2a",
+    "url": "./static/js/main.62710e2c.chunk.js"
+  },
+  {
+    "revision": "1c39d98e568e275b380b",
+    "url": "./static/js/runtime-main.a9e20631.js"
+  }
+]);
