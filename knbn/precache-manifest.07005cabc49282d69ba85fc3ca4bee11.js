a0bffc7c12b147e1c0877bb625a5ded858f3cc0c
shellyln <shellyln@users.noreply.github.com>
2019-10-04 05:49:02 +0900

    update app

---
+++ knbn/precache-manifest.07005cabc49282d69ba85fc3ca4bee11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822b02148e066366b01df43d25632d02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "703397e2ecff76130d85",
+    "url": "./static/css/2.5abeca67.chunk.css"
+  },
+  {
+    "revision": "9cc1dc4e48dd3f90554c",
+    "url": "./static/css/main.329740d7.chunk.css"
+  },
+  {
+    "revision": "703397e2ecff76130d85",
+    "url": "./static/js/2.ec2bb225.chunk.js"
+  },
+  {
+    "revision": "9cc1dc4e48dd3f90554c",
+    "url": "./static/js/main.4210fa20.chunk.js"
+  },
+  {
+    "revision": "1c39d98e568e275b380b",
+    "url": "./static/js/runtime-main.a9e20631.js"
+  }
+]);
