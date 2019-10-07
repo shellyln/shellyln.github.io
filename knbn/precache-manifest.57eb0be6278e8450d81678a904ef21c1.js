170b3de5dead68edf1612d4923120985a6cf245a
shellyln <shellyln@users.noreply.github.com>
2019-10-08 00:32:46 +0900

    kanban-board-app v0.1.9

--- knbn/precache-manifest.57eb0be6278e8450d81678a904ef21c1.js
+++ knbn/precache-manifest.57eb0be6278e8450d81678a904ef21c1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1bd7280f8f9c920b4b2485601d37131",
+    "revision": "dde5a67a568c6e841bc8701d8b3030e4",
     "url": "./index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.5abeca67.chunk.css"
   },
   {
-    "revision": "7372f17aa6e4bc312181",
-    "url": "./static/css/main.329740d7.chunk.css"
+    "revision": "e582d55ca362a95f4e48",
+    "url": "./static/css/main.52d77a92.chunk.css"
   },
   {
     "revision": "9477c874e0a1ece0e545",
     "url": "./static/js/2.3f9bf4ff.chunk.js"
   },
   {
-    "revision": "7372f17aa6e4bc312181",
-    "url": "./static/js/main.c99e7783.chunk.js"
+    "revision": "e582d55ca362a95f4e48",
+    "url": "./static/js/main.db2caab8.chunk.js"
   },
   {
     "revision": "d75709ac66634509db91",
