8e25f7050dd3a33a0eeaff5d8d5c85c5d154d403
shellyln <shellyln@users.noreply.github.com>
2019-12-01 08:24:14 +0900

    Update app (staging)

---
+++ knbn-stg/precache-manifest.d41ac0f75045c7f48a50bccc095c30b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3373bbc6f9e1c5002bf444d3ba94c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8dd3b317f358b9f22809",
+    "url": "./static/css/2.5abeca67.chunk.css"
+  },
+  {
+    "revision": "7c6f84c4a25cf6c61c9d",
+    "url": "./static/css/main.e4ea5e15.chunk.css"
+  },
+  {
+    "revision": "8dd3b317f358b9f22809",
+    "url": "./static/js/2.6cd1d11d.chunk.js"
+  },
+  {
+    "revision": "7c6f84c4a25cf6c61c9d",
+    "url": "./static/js/main.efe9d68f.chunk.js"
+  },
+  {
+    "revision": "d75709ac66634509db91",
+    "url": "./static/js/runtime-main.efdeb34d.js"
+  }
+]);
